c2bc1a829be2b9c0bbea2be986690cae050924ff
ManojKumarKorrapati <S545711@nwmissouri.edu>
2021-11-12 09:13:45 -0600

    put and delete

--- controllers/tea.js
+++ controllers/tea.js
@@ -1,13 +1,26 @@
 var Tea = require("../models/tea");
 
 // List of all teas
-exports.tea_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: tea list");
+exports.tea_list = async function (req, res) {
+  try {
+    theteas = await Tea.find();
+    res.send(theteas);
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
 
 // for a specific tea.
-exports.tea_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: tea detail: " + req.params.id);
+exports.tea_detail = async function (req, res) {
+  console.log("detail"  + req.params.id) 
+    try { 
+        result = await Tea.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 };
 
 // Handle tea create on POST.
@@ -30,24 +43,37 @@ exports.tea_create_post = async function (req, res) {
 };
 
 // Handle tea delete form on DELETE.
-exports.tea_delete = function (req, res) {
-  res.send("NOT IMPLEMENTED: tea delete DELETE " + req.params.id);
+exports.tea_delete =async function (req, res) {
+  console.log("delete "  + req.params.id) 
+    try { 
+        result = await Tea.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 };
 
 // Handle tea update form on PUT.
-exports.tea_update_put = function (req, res) {
-  res.send("NOT IMPLEMENTED: tea update PUT" + req.params.id);
-};
-
-// List of all teas
-exports.tea_list = async function (req, res) {
-  try {
-    theteas = await Tea.find();
-    res.send(theteas);
-  } catch (err) {
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-  }
+exports.tea_update_put =async function (req, res) {
+  console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Tea.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.costume_type)  
+               toUpdate.tea_brand = req.body.tea_brand; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 };
 
 // VIEWS
@@ -62,5 +88,3 @@ exports.tea_view_all_Page = async function(req, res) {
       res.send(`{"error": ${err}}`); 
   }   
 }; 
-
-
